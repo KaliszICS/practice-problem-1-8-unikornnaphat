6fa5257d8da27c1bfca5d783d85b5c942b624ae4
unikornnaphat <350364092@tdsb.ca>
2026-03-02 16:34:28 +0000

    1.8 practice problem-sandy

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,11 +5,11 @@
  * Date Last Edited: March 29, 2019
  */
 
-import java.until.Scanner;
+import java.util.Scanner;
 
 public class PracticeProblem {
 
-	static Scanner s = new Scanner();
+	static Scanner s = new Scanner(System.in);
   	public static void main(String[] args) {
 		
 		q1();
@@ -32,17 +32,17 @@ public class PracticeProblem {
 		// From input recieve two integers.  Output the quotient rounded down.
 		Scanner s = new Scanner(System.in);
 		System.out.print("Input a number: ");
-		int num1 = s.nextLine();
+		int num1 =  Integer.parseInt(s.nextLine());
 		System.out.print("Input a number: ");
-		int num2 = s.nextLine();
-		system.out.println(num1 / num2);
+		int num2 = Integer.parseInt(s.nextLine());
+		System.out.println(num1 / num2);
 		s.close();
 	}
 
 	public static void q3() {
 		// Output the phrase "hello "Mr. Kalisz" have you seen my work yet?"
 		
-		System.out.println(" "\Mr. Kalisz\" have you seen my work yet?");
+		System.out.println(" \"hello \\Mr. Kalisz\" have you seen my work yet?");
 	}
 
 	public static void q4() {
@@ -53,7 +53,7 @@ public class PracticeProblem {
 		double num1 = s.nextInt();
 		System.out.print("Input a number: ");
 		double num2 = s.nextInt();
-		System.out.println(num1 + 2 * num2)
+		System.out.println(num1 + 2 * num2);
 		s.close();
 	}
 }
